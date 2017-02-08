4b27f86bfd5079d8c4d0b511ccde5d80af2d7fe8
prudelou <pierre.rudelou@hotmail.fr>
2017-02-08 17:24:25 +0100

    Add Getters and Setters in Controller

--- Crawler/src/crawler/Controller.java
+++ Crawler/src/crawler/Controller.java
@@ -29,6 +29,168 @@ import javafx.scene.input.ContextMenuEvent;
 public class Controller {
 	@FXML
 	private Tab tabCrawl;
+	public Tab getTabCrawl() {
+		return tabCrawl;
+	}
+	public void setTabCrawl(Tab tabCrawl) {
+		this.tabCrawl = tabCrawl;
+	}
+	public Label getLabelURL() {
+		return labelURL;
+	}
+	public void setLabelURL(Label labelURL) {
+		this.labelURL = labelURL;
+	}
+	public Label getLabelLien() {
+		return labelLien;
+	}
+	public void setLabelLien(Label labelLien) {
+		this.labelLien = labelLien;
+	}
+	public TextField getTextFielURL() {
+		return textFielURL;
+	}
+	public void setTextFielURL(TextField textFielURL) {
+		this.textFielURL = textFielURL;
+	}
+	public CheckBox getCheckBoxLien() {
+		return checkBoxLien;
+	}
+	public void setCheckBoxLien(CheckBox checkBoxLien) {
+		this.checkBoxLien = checkBoxLien;
+	}
+	public CheckBox getCheckBoxIV() {
+		return checkBoxIV;
+	}
+	public void setCheckBoxIV(CheckBox checkBoxIV) {
+		this.checkBoxIV = checkBoxIV;
+	}
+	public Label getLabelProfondeur() {
+		return labelProfondeur;
+	}
+	public void setLabelProfondeur(Label labelProfondeur) {
+		this.labelProfondeur = labelProfondeur;
+	}
+	public TextField getTextFieldProfondeur() {
+		return textFieldProfondeur;
+	}
+	public void setTextFieldProfondeur(TextField textFieldProfondeur) {
+		this.textFieldProfondeur = textFieldProfondeur;
+	}
+	public Button getButtonDownload() {
+		return buttonDownload;
+	}
+	public void setButtonDownload(Button buttonDownload) {
+		this.buttonDownload = buttonDownload;
+	}
+	public Label getLabelLangue1() {
+		return labelLangue1;
+	}
+	public void setLabelLangue1(Label labelLangue1) {
+		this.labelLangue1 = labelLangue1;
+	}
+	public ChoiceBox getChoiceBoxLangue1() {
+		return choiceBoxLangue1;
+	}
+	public void setChoiceBoxLangue1(ChoiceBox choiceBoxLangue1) {
+		this.choiceBoxLangue1 = choiceBoxLangue1;
+	}
+	public Label getLabelRepertoire() {
+		return labelRepertoire;
+	}
+	public void setLabelRepertoire(Label labelRepertoire) {
+		this.labelRepertoire = labelRepertoire;
+	}
+	public TextField getTextFieldRepertoire() {
+		return textFieldRepertoire;
+	}
+	public void setTextFieldRepertoire(TextField textFieldRepertoire) {
+		this.textFieldRepertoire = textFieldRepertoire;
+	}
+	public Button getButtonParcourir() {
+		return buttonParcourir;
+	}
+	public void setButtonParcourir(Button buttonParcourir) {
+		this.buttonParcourir = buttonParcourir;
+	}
+	public Label getLabelDownload1() {
+		return labelDownload1;
+	}
+	public void setLabelDownload1(Label labelDownload1) {
+		this.labelDownload1 = labelDownload1;
+	}
+	public ProgressBar getProgressBarDownload1() {
+		return progressBarDownload1;
+	}
+	public void setProgressBarDownload1(ProgressBar progressBarDownload1) {
+		this.progressBarDownload1 = progressBarDownload1;
+	}
+	public ProgressIndicator getProgressIndicatorDownload() {
+		return progressIndicatorDownload;
+	}
+	public void setProgressIndicatorDownload(ProgressIndicator progressIndicatorDownload) {
+		this.progressIndicatorDownload = progressIndicatorDownload;
+	}
+	public Tab getTabVisualisation() {
+		return tabVisualisation;
+	}
+	public void setTabVisualisation(Tab tabVisualisation) {
+		this.tabVisualisation = tabVisualisation;
+	}
+	public Label getLabelLangue2() {
+		return labelLangue2;
+	}
+	public void setLabelLangue2(Label labelLangue2) {
+		this.labelLangue2 = labelLangue2;
+	}
+	public ChoiceBox getChoiceBoxLangue2() {
+		return choiceBoxLangue2;
+	}
+	public void setChoiceBoxLangue2(ChoiceBox choiceBoxLangue2) {
+		this.choiceBoxLangue2 = choiceBoxLangue2;
+	}
+	public Label getLabelDownload2() {
+		return labelDownload2;
+	}
+	public void setLabelDownload2(Label labelDownload2) {
+		this.labelDownload2 = labelDownload2;
+	}
+	public ProgressBar getProgressBarDownload2() {
+		return progressBarDownload2;
+	}
+	public void setProgressBarDownload2(ProgressBar progressBarDownload2) {
+		this.progressBarDownload2 = progressBarDownload2;
+	}
+	public ProgressIndicator getProgressIndicator2() {
+		return progressIndicator2;
+	}
+	public void setProgressIndicator2(ProgressIndicator progressIndicator2) {
+		this.progressIndicator2 = progressIndicator2;
+	}
+	public TreeView getTreeView() {
+		return treeView;
+	}
+	public void setTreeView(TreeView treeView) {
+		this.treeView = treeView;
+	}
+	public Button getButtonOuvrir() {
+		return buttonOuvrir;
+	}
+	public void setButtonOuvrir(Button buttonOuvrir) {
+		this.buttonOuvrir = buttonOuvrir;
+	}
+	public Button getButtonSupprimer() {
+		return buttonSupprimer;
+	}
+	public void setButtonSupprimer(Button buttonSupprimer) {
+		this.buttonSupprimer = buttonSupprimer;
+	}
+	public ScrollPane getScrollPane() {
+		return scrollPane;
+	}
+	public void setScrollPane(ScrollPane scrollPane) {
+		this.scrollPane = scrollPane;
+	}
 	@FXML
 	private Label labelURL;
 	@FXML
@@ -82,7 +244,7 @@ public class Controller {
 	@FXML
 	private ScrollPane scrollPane;
 
-	// Event Listener on CheckBox[#checkBoxLien].onMouseClicked
+	// Event Listener on CheckBox[#checkBoxLien].onMouseClicked 
 	@FXML
 	public void listenerCheckBox(MouseEvent event) {
 		// TODO Autogenerated
