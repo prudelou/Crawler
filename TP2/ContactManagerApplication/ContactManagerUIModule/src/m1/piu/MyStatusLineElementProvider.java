a82187e8c3833d0cefd49129a6ab6e0bdb8a0636
prudelou <pierre.rudelou@hotmail.fr>
2017-04-03 17:18:55 +0200

    Finish Question 2

--- TP2/ContactManagerApplication/ContactManagerUIModule/src/m1/piu/MyStatusLineElementProvider.java
+++ TP2/ContactManagerApplication/ContactManagerUIModule/src/m1/piu/MyStatusLineElementProvider.java
@@ -4,18 +4,43 @@
  * and open the template in the editor.
  */
 package m1.piu;
-
+import java.awt.Button;
+import javafx.fxml.FXML;
 import java.awt.Component;
+import java.awt.Label;
+import java.io.IOException;
+import java.net.URL;
+import javafx.embed.swing.JFXPanel;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import org.openide.awt.StatusLineElementProvider;
+import org.openide.util.Exceptions;
+import org.openide.util.lookup.ServiceProvider;
 
 /**
  *
  * @author pierre
  */
-public class MyStatusLineElementProvider implements org.openide.awt.StatusLineElementProvider {
+@ServiceProvider(service = StatusLineElementProvider.class, position = 100)
+public class MyStatusLineElementProvider implements StatusLineElementProvider{
 
     @Override
     public Component getStatusLineElement() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        JFXPanel fxPanel = new JFXPanel();
+        URL url = getClass().getResource("/m1/piu/FXMLMyStatusBar.fxml");
+        FXMLLoader loader = new FXMLLoader(url);
+        try
+        {
+            Parent root=(Parent)loader.load();
+            Scene scene=new Scene(root);
+            fxPanel.setScene(scene);
+        }
+        catch(IOException e)
+        {
+            Exceptions.printStackTrace(e);
+        }
+        return fxPanel;
     }
     
 }
